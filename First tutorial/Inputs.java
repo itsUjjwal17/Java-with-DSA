91132d785b472cc56976b75e487e1440b6715b6a
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-01-19 01:20:30 +0530

    new commit

--- First tutorial/Inputs.java
+++ First tutorial/Inputs.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; //importing Scanner library from java utility
 public class Inputs{
     public static void main(String[] args){
-        Inputs sc=new Scanner(System.in); //using sc as variable //taking input or scan input from System
+        Scanner sc=new Scanner(System.in); //using sc as variable //taking input or scan input from System
         int a=sc.nextInt(); //taking integer input
         System.out.println(a);
         //Scanner is basically a class that specifies an input stream and using the object of that class and variable we can take input 
