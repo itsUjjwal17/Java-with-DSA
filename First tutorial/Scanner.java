1727963a690d5d0959660da3dd4a07904bcbc163
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-01-19 00:41:47 +0530

    new commit

---
+++ First tutorial/Scanner.java
@@ -0,0 +1,7 @@
+import java.util.Scanner; //importing Scanner library from java utility
+public class Scanner {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in); //using sc as variable //taking input or scan input from System
+        int a=sc.nextInt(); //taking integer input
+    }
+}
