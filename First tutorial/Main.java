e0bf7e402734aae0bab9f89161f05607e21b6cd3
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-01-18 22:59:26 +0530

    first commit

--- First tutorial/Main.java
+++ First tutorial/Main.java
@@ -1,4 +1,4 @@
-public class Main{ //Here public means it can be accessed from anywhere in the code
+class Main{ //Here public means it can be accessed from anywhere in the code
     public static void main(String[] args){ //main function of java code
         System.out.println("Hello World!");
     }
