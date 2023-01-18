cb941b807cbdd7e71d64ca6e64540c6ccce755d2
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-01-18 22:52:28 +0530

    starting my java with DSA

---
+++ First tutorial/Main.java
@@ -0,0 +1,5 @@
+public class Main{ //Here public means it can be accessed from anywhere in the code
+    public static void main(String[] args){ //main function of java code
+        System.out.println("Hello World!");
+    }
+}
