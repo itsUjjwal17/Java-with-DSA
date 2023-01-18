3795d600fcb23800e6c530396e6fd8c6d10159bb
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-01-19 01:12:11 +0530

    new commit

---
+++ First tutorial/Primitives.java
@@ -0,0 +1,13 @@
+public class Primitives {
+    public static void main(String[] args){
+        int rollno=69; //primitive is any datatype that you cannot break even further
+        // here int is a primitive datatype
+        char letter='r';
+        float marks=98.6f;
+        double largeDecimalnumbers=48456515.448494;
+        long largeInteger=548465158486115L;
+        boolean check=true; //or false
+        //String is not a prmitive datatype
+        String name="Ujjwal";
+    }
+}
