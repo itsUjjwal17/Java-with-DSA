8ec7c4226e0881f5482cc00fd4fd32f1bc9e3504
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-01-20 23:40:20 +0530

    new commit

---
+++ Arrays/ArrayListExample.java
@@ -0,0 +1,8 @@
+public class ArrayListExample {
+    public static void main(String[] args) {
+        //Syntax
+        /*
+         ArrayList<Integer> list=new ArrayList<Integer>(); //writing Integer again after equals to is not mandatory
+         */
+    }
+}
