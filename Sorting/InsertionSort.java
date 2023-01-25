46cde794b0b329bd9ac08e2fd44bce1b318de7ae
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-01-25 23:44:25 +0530

    new commit

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -6,8 +6,8 @@ public class InsertionSort {
         System.out.println(Arrays.toString(arr));
     }
     static void Sort(int[] arr){
-        for(int i;i<arr.length-1;i++){
-            for(int j=i+1;j>0;j++){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j>0;j--){
                 if(arr[j]<arr[j-1]){
                     swap(arr,j,j-1);
                 }else{
