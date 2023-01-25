eeacdf5e86dba7571e548d4b48bb34be50c60f26
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-01-26 04:22:31 +0530

    new commit

---
+++ Sorting/CyclicSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+public class CyclicSort {   
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,1,4};
+        Sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    //only applicable when the array elements is from 0 to N continously
+    static void Sort(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            }else{
+                i++;
+            }
+        }
+    }
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
