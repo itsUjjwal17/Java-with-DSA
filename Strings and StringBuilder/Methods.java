aa2f2f1b1845ff30af06ccda0c9806cfb75f8df8
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-01-28 23:24:32 +0530

    new commit

--- Strings and StringBuilder/Methods.java
+++ Strings and StringBuilder/Methods.java
@@ -1,4 +1,8 @@
+import java.net.SocketTimeoutException;
 import java.util.Arrays;
+
+import javax.imageio.spi.RegisterableService;
+import javax.swing.plaf.synth.Region;
 
 public class Methods {
     public static void main(String[] args) {
@@ -6,5 +10,10 @@ public class Methods {
         System.out.println(Arrays.toString(name.toCharArray()));
         System.out.println(name.toLowerCase());
         System.out.println(name); //original one will not change cuz its immutable
+
+        System.out.println(name.indexOf('a'));
+
+        System.out.println("     Ujjwal   ".strip());
+        System.out.println(Arrays.toString(name.split(" "))); //cuts wherever it find space
     }
 }
