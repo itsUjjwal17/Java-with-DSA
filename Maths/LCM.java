b9d77e78d5d0a41a76b9c3f408a398f1654b8e68
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-02-04 14:37:20 +0530

    new commit

---
+++ Maths/LCM.java
@@ -0,0 +1,14 @@
+public class LCM {
+    public static void main(String[] args) {
+        System.out.println(lcm(4,10));
+    }
+    static int gcd(int a, int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+    static int lcm(int a, int b){
+        return a*b/gcd(a,b);
+    }
+}
