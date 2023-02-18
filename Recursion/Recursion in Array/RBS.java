9bb95e958e4a0f2a190774ee59e3840f1b85b8da
Ujjwal <ujjwalkrishna123321@gmail.com>
2023-02-19 00:00:28 +0530

    new commit

--- Recursion/Recursion in Array/RBS.java
+++ Recursion/Recursion in Array/RBS.java
@@ -1,7 +1,7 @@
 public class RBS {
     public static void main(String[] args) {
         int[] arr={5,6,7,8,9,1,2,3};
-        System.out.println(search(arr, 6, 0, arr.length-1));
+        System.out.println(search(arr, 1, 0, arr.length-1));
     }
 
     static int search(int[] arr, int target, int s, int e){
